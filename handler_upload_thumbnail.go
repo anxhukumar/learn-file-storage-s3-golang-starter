4c2dc2f015024f9dfba2a7842e838797606ca6a4
anxhukumar <contact.anshukumar@protonmail.com>
2025-11-18 16:16:02 +0530

    add thumbnail update in sqlite db in base64

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +30,52 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+
+	mediaType := header.Header.Get("Content-Type")
+
+	imageData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to read image data", err)
+		return
+	}
+
+	// get video metadata
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to get video from database", err)
+		return
+	}
+
+	// check if it belongs to the currently logged in user
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Access denied to video as the user is not the owner", err)
+		return
+	}
+
+	// convert image data to base64 string
+	imageBase64Data := base64.StdEncoding.EncodeToString(imageData)
+
+	imageDataUrl := fmt.Sprintf("data:%v;base64,%v", mediaType, imageBase64Data)
+
+	//update video thumbnail url
+	video.ThumbnailURL = &imageDataUrl
+
+	// update video
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Can't update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
